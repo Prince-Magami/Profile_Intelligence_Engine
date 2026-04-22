49278160bb816b3e74579e089f3f4eaad17e5a1d
Abubakar Muhammad Magami <magamiabu@gmail.com>
2026-04-22 05:56:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,66 @@
 require("dotenv").config();
 
-const express=require("express");
-const cors=require("cors");
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
-const connectDB=require("./db");
+const connectDB = require("./db");
+const Profile = require("./models/Profile"); //
+const profileRoutes = require("./routes/profiles");
 
-const profileRoutes=
-require("./routes/profiles");
-
-const app=express();
+const app = express();
 
 connectDB();
 
 app.use(cors());
-
 app.use(express.json());
 
-app.get("/",(req,res)=>{
- res.json({
-   status:"success",
-   message:"Intelligence Query Engine running"
- });
+app.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "Intelligence Query Engine running"
+  });
 });
 
-app.use("/api/profiles",profileRoutes);
 
-const PORT=
-process.env.PORT || 8080;
+app.get("/seed", async (req, res) => {
+  try {
+    const filePath = path.join(__dirname, "data", "profiles-2026.json");
+    const rawData = fs.readFileSync(filePath);
+    const profiles = JSON.parse(rawData);
 
-app.listen(PORT,()=>{
- console.log(
-  `Server running on ${PORT}`
- );
+    const existingCount = await Profile.countDocuments();
+    if (existingCount > 0) {
+      return res.json({
+        status: "success",
+        message: "Database already seeded",
+        total: existingCount
+      });
+    }
+
+    await Profile.insertMany(profiles);
+
+    res.json({
+      status: "success",
+      message: "Database seeded successfully",
+      total: profiles.length
+    });
+
+  } catch (error) {
+    console.error("SEED ERROR:", error.message);
+
+    res.status(500).json({
+      status: "error",
+      message: "Failed to seed database"
+    });
+  }
+});
+
+app.use("/api/profiles", profileRoutes);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
